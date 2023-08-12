dd5f829fd485d697520d4e2025f6b1f4bdb34a06
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-08-12 19:17:40 +0000

    Update frontend/deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,7 +2,10 @@
 set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -f /home/student/sausage-store.jar||true
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-frontend-${VERSION}.jar ${NEXUS_REPO_URL_BACKEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp ./sausage-store-frontend-${VERSION}.jar /home/front-user/sausage-store.jar||true
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-frontend-${VERSION}.jar ${NEXUS_REPO_URL_BACKEND}/sausage-store-front/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+ls -la
+tar -xvf sausage-store-frontend-${VERSION}.tar.gz
+ls -la
+sudo cp ./sausage-store-frontend-${VERSION}/builds/std-019-002/sausage-store/frontend/dist/frontend /home/front-user/sausage-store/||true
 sudo systemctl daemon-reload 
 sudo systemctl restart sausage-store-frontend.service
