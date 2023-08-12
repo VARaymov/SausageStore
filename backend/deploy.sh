88f08c3da5ff863dacc3ced6a0f2100e91e4ce9d
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-08-12 12:55:59 +0000

    Update backend/deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,5 +3,7 @@
 set -xe
 #скачиваем артефакт
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-backend-${VERSION}.jar ${NEXUS_REPO_URL_BACKEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+pwd
+ls -la
 #запускаем java-приложение в фоне
 nohup "java -jar sausage-store-${VERSION}.jar --server.port 9090 $"
