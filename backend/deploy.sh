19887b051f69602eb4bc758b4b92e4cd3e2497f0
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-08-13 08:50:36 +0000

    Update backend/deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,8 +7,6 @@ sudo rm -f /home/student/sausage-store-backend-${VERSION}.jar||true
 #Переносим в нужную папку
 #скачиваем артефакт
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-backend-${VERSION}.jar ${NEXUS_REPO_URL_BACKEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-pwd
-ls -la
 sudo cp ./sausage-store-backend-${VERSION}.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится - продолжай
 #Перезапускаем сервис сосисочной
 sudo systemctl daemon-reload 
