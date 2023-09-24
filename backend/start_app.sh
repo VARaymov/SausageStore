828d0378d5e741c96d8d5090e60c3077b1c3538b
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-09-24 09:47:37 +0000

    Update backend/start_app.sh

--- backend/start_app.sh
+++ backend/start_app.sh
@@ -7,7 +7,7 @@ source /home/student/variables.env
 echo "$PSQL_HOST $PSQL_PORT $PSQL_DBNAME $PSQL_USER $PSQL_PASSWORD" > ~/12345.txt
 
 # Запуск Java-приложения
-/usr/bin/java -jar /home jarservice/sausage-store.jar \
+/usr/bin/java -jar /home/jarservice/sausage-store.jar \
   --spring.datasource.url=jdbc:postgresql://$PSQL_HOST:$PSQL_PORT/$PSQL_DBNAME \
   --spring.datasource.username=$PSQL_USER \
   --spring.datasource.password=$PSQL_PASSWORD \
