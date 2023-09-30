989608880617a95432e3cb86009e759df6fb0b5c
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-09-30 20:47:03 +0000

    Update backend/backend_deploy.sh, backend/.gitlab-ci.yml

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -8,7 +8,6 @@ SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
 docker network create -d bridge sausage_network || true
 echo "$CI_REGISTRY_PASSWORD" | docker login --username "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY"
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker pull gitlab.praktikum-services.ru:5050/std-019-002/sausage-store/sausage-backend:latest
 docker stop backend || true
 docker rm backend || true
