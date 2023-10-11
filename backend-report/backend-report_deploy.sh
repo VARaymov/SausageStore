3e9633455b42ac8ffbbafa016f6ac7c09e399d8d
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-10-11 20:27:40 +0000

    Update backend-report/backend-report_deploy.sh

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -1,11 +1,11 @@
 set +e
 cat > backend-report.env <<EOF
-SPRING_MONGODB_URI=${SPRING_MONGODB_URI}
+REPORTS_MONGODB_URI=${SPRING_MONGODB_URI}
 SPRING_CLOUD_VAULT_ENABLED=TRUE
 EOF
 echo "$CI_REGISTRY_PASSWORD" | docker login --username "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY"
 docker pull gitlab.praktikum-services.ru:5050/std-019-002/sausage-store/sausage-backend-report:latest
 docker-compose stop backend-report || true
-docker-compose rm backend-report || true
+docker-compose rm -f backend-report || true
 set -e
 docker-compose --env-file backend-report.env up -d backend-report
