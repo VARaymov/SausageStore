710f0af0dc1549ede1fb28f97cdc8cd89872aae8
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-10-11 18:59:48 +0000

    Update backend-report/backend-report_deploy.sh, backend-report/.gitlab-ci.yml, docker-compose.yml, backend-report/Dockerfile

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -4,6 +4,7 @@ DB=${SPRING_MONGODB_URI}
 SPRING_CLOUD_VAULT_ENABLED=TRUE
 EOF
 docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD} ${REGISTRY}
+docker-compose stop backend-report || true
 docker-compose rm backend-report || true
 set -e
-docker compose --env-file backend-report.env --pull always --wait backend-report -d
+docker-compose --env-file backend-report.env up backend-report -d
