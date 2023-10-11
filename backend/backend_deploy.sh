30436ea494c2246ece38551df36f662b2e7a4a19
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-10-11 18:31:46 +0000

    Update backend/backend_deploy.sh, docker-compose.yml, frontend/.gitlab-ci.yml, backend/.gitlab-ci.yml, frontend/frontend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -6,14 +6,9 @@ SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
-echo $SPRING_DATASOURCE_URL
-echo $SPRING_DATASOURCE_USERNAME
-echo $SPRING_DATASOURCE_PASSWORD
-echo $SPRING_DATA_MONGODB_URI
 echo "$CI_REGISTRY_PASSWORD" | docker login --username "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY"
 docker pull gitlab.praktikum-services.ru:5050/std-019-002/sausage-store/sausage-backend:latest
 docker-compose stop backend || true
 docker-compose rm backend || true
 set -e
-#docker-compose --env-file backend.env up --pull always --wait -d backend
-docker-compose --env-file backend.env up  -d backend
+docker-compose --env-file backend.env up -d backend
