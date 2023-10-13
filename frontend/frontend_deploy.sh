368f5b64b75ce443ab2ce8dd4672a64fb5cdd019
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-10-13 19:22:49 +0000

    Update frontend/frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
 echo "$CI_REGISTRY_PASSWORD" | docker login --username "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY"
-docker pull gitlab.praktikum-services.ru:5050/std-019-002/sausage-store/sausage-frontend:latest
+docker pull gitlab.praktikum-services.ru:5050/std-019-002/sausage-store/sausage-frontend-nginx:latest
 docker-compose stop frontend || true
 docker-compose rm -f frontend || true
 set -e
