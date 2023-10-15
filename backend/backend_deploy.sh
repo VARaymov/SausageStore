ac1b1cc26ef3448bda00933fde40bf4f7921add8
std-019-002 std-019-002 <std-019-002@praktikum-services.ru>
2023-10-15 11:35:44 +0000

    Update backend/pom.xml, docker-compose.yml, backend/backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -7,7 +7,30 @@ SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 EOF
 echo "$CI_REGISTRY_PASSWORD" | docker login --username "$CI_REGISTRY_USER" --password-stdin "$CI_REGISTRY"
 docker pull gitlab.praktikum-services.ru:5050/std-019-002/sausage-store/sausage-backend:latest
-docker-compose stop backend || true
-docker-compose rm -f backend || true
-set -e
-docker-compose --env-file backend.env up -d --scale backend=2
+CURRENT_CONTAINER="0"
+if docker ps --filter "name=blue-backend" -q; then
+    CURRENT_CONTAINER="blue-backend"
+	echo "Запущен blue-backend, будет запущен green-backend"
+	docker-compose --env-file backend.env up -d green-backend
+	sleep 15
+	HEALTHY=false
+	while [ "$HEALTHY" != "true" ]; do
+		HEALTHY=$(docker inspect -f "{{.State.Status}}" green-backend)
+		sleep 15
+		if [ "$HEALTHY" == "exited" ]; then
+			echo "Не удалось запустить green-backend"
+			exit 1
+		fi
+	done
+	echo "green-backend запущен успешно, blue-backend будет остановлен"
+	docker-compose stop blue-backend
+	exit 0
+elif docker ps --filter "name=green-backend" -q; then
+    CURRENT_CONTAINER="green-backend"
+	echo "Уже запущен green-backend"
+	exit 0
+else
+    docker-compose --env-file backend.env up -d green-backend
+	echo "Не запущено ни одного контейнера, будет запущен green-backend"
+	exit 0
+fi
